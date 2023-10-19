145c38d9f126b86c8b17e177f31ae0406c9ce637
Gilbert Ardila <gilbertferney@gmail.com>
2023-10-19 11:31:11 -0500

    se agrega documentación con swagger inicialmente al endpoint de users

--- index.js
+++ index.js
@@ -6,12 +6,44 @@ const {
   boomErrorHandler,
   ormErrorHandler
 } = require('./src/middlewares/error.handler');
+
+//swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+//cors
 const cors = require('cors');
+
+// swagger specifications
+const swaggerSpec= {
+  definition:{
+    openapi:'3.0.0',
+    info:{
+      title:'API para manerjar las reservaciones de un Hotel',
+      version:'0.0.1',
+      description:'Hotel API',
+      contact:{
+        name:'Gilbert Ardila',
+        email:'gilbertferney@gmail.com'
+      },
+      servers:[
+        {
+          url:'http://localhost:3001'
+        }
+      ]
+    }
+  },
+  apis:['./src/routes/*.js']
+
+}
 
 
 const app = express()
 const port = process.env.PORT || 3001
 app.use(express.json())
+
+// swagger middleware
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerJsDoc(swaggerSpec)));
 
 //cors whitelist
 const whitelist = ['http://localhost:3001', 'https://myapp.co'];
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -12,7 +12,56 @@ const {
 const router = express.Router();
 const service = new UserService();
 
-//get all users
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      properties:
+ *        nickname:
+ *          type: string
+ *          description: the user nickname
+ *        email:
+ *          type: string
+ *          description: the user mail adress
+ *        password:
+ *          type: string
+ *          description: the user password must be maximum 8 digits
+ *        role:
+ *          type: string
+ *          description: it could be or admin or user, it will be user by default
+ *        recoveryToken:
+ *          type: string
+ *          description: null by default
+ *      required:
+ *        - nickname
+ *        - email
+ *        - password
+ *      example:
+ *        nickname: chepe
+ *        email: chepe@gmail.com
+ *        password: chepe123
+ */
+
+
+/**
+ * @swagger
+ * /api/v1/users:
+ *  get:
+ *    summary: return all users
+ *    tags: [User]
+ *    responses:
+ *      200:
+ *        description: returns all users
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/User'
+ */
+
 router.get("/", async (req, res, next) => {
   try {
     const users = await service.find();
@@ -22,7 +71,31 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// get one user by id
+/**
+ * @swagger
+ * /api/v1/users/{id}:
+ *  get:
+ *    summary: return the specific user according with the id
+ *    tags: [User]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: user id
+ *    responses:
+ *      200:
+ *        description: returns one user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/User'
+ *      404:
+ *        description: User not found
+ */
+
 router.get(
   "/:id",
   validatorHandler(getUserSchema, "params"),
@@ -37,7 +110,30 @@ router.get(
   }
 );
 
-//get one user by email
+/**
+ * @swagger
+ * /api/v1/users/email/{email}:
+ *  get:
+ *    summary: return the specific user according with the email
+ *    tags: [User]
+ *    parameters:
+ *      - in: path
+ *        name: email
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: user mail
+ *    responses:
+ *      200:
+ *        description: returns one user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/User'
+ *      404:
+ *        description: Sorry we can not find any email coincidence in our data base, please check your email again
+ */
 router.get("/email/:email", async (req, res, next) => {
   try {
     const { email } = req.params;
@@ -48,7 +144,30 @@ router.get("/email/:email", async (req, res, next) => {
   }
 });
 
-//get users by role
+/**
+ * @swagger
+ * /api/v1/users/role/{role}:
+ *  get:
+ *    summary: return the specific user according with the role
+ *    tags: [User]
+ *    parameters:
+ *      - in: path
+ *        name: role
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: user role
+ *    responses:
+ *      200:
+ *        description: returns all users with that specific role
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/User'
+ *      404:
+ *        description: Sorry we can not find any user with that specific role, please check it again
+ */
 router.get("/role/:role", async (req, res, next) => {
   try {
     const { role } = req.params;
@@ -59,7 +178,23 @@ router.get("/role/:role", async (req, res, next) => {
   }
 });
 
-//create a new user
+/**
+ * @swagger
+ * /api/v1/users:
+ *  post:
+ *    summary: this is the endpoint to create a user
+ *    tags: [User]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: returns the new user data
+ */
 router.post(
   "/",
   validatorHandler(createUserSchema, "body"),
@@ -74,7 +209,38 @@ router.post(
   }
 );
 
-//update a user
+/**
+ * @swagger
+ * /api/v1/users/{id}:
+ *  patch:
+ *    summary: update one speciffic user according to the id
+ *    tags: [User]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/User'
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: user id
+ *    responses:
+ *      200:
+ *        description: returns the user info updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/User'
+ *      404:
+ *        description: User not found
+ */
+
 router.patch(
   "/:id",
   validatorHandler(getUserSchema, "params"),
@@ -91,7 +257,30 @@ router.patch(
   }
 );
 
-//delete a user
+/**
+ * @swagger
+ * /api/v1/users/{id}:
+ *  delete:
+ *    summary: delete one speciffic user according to the id
+ *    tags: [User]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: user id
+ *    responses:
+ *      200:
+ *        description: User with id ${id} deleted
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#/components/schemas/User'
+ *      404:
+ *        description: User not found
+ */
 router.delete(
   "/:id",
   validatorHandler(deleteUserSchema, "params"),
