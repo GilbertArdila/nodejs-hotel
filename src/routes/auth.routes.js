c37ec372ebc3a99ca3fcd69ac3090fa44e71ce66
Gilbert Ardila <gilbertferney@gmail.com>
2023-11-01 20:00:03 -0500

    generacion del token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const {config} = require('../config/config'); 
 const validatorHandler = require("../middlewares/validator.handler");
 const {loginSchema} = require('../schemas/login.dto');
 
@@ -13,7 +16,28 @@ passport.authenticate('local',{session:false}),
 
 async(req, res, next) => {
     try {
-        res.json(req.user)
+        //recuperamos el usuario del request desde el local strategy
+        const user = req.user
+
+        //creamos el payload
+        const payload ={
+           sub: user.id,
+           role:user.role
+        }
+
+        //creamos el token en el cual por seguridad solo enviamos el rol y el id del usuario
+        const token = jwt.sign(payload,config.jwtSecret)
+
+        //borramos estos datos de la respuesta
+        delete user.dataValues.createdAt
+        delete user.dataValues.updatedAt
+
+        //retornamos el ususario y el token
+        res.json(
+            {user,
+             token
+            }
+        )
     } catch (error) {
         next(error)
     }
